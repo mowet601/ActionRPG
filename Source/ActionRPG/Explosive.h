7f678b55909d8b54d868d8931b67d43af852cf05
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2020-07-26 16:48:00 +0200

    Pickups

---
+++ Source/ActionRPG/Explosive.h
@@ -0,0 +1,23 @@
+// Copyright by Hakan Akkurt
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Explosive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_API AExplosive : public AItem
+{
+	GENERATED_BODY()
+public:
+	
+	AExplosive();
+
+    virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+    virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+};
