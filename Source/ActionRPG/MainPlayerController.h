bab9323748abb569db1616cf5bc5f2f9f72bbf46
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2020-08-16 03:46:39 +0200

    Health and Stamina Bar added

---
+++ Source/ActionRPG/MainPlayerController.h
@@ -0,0 +1,29 @@
+// Copyright by Hakan Akkurt
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+
+	// Reference to the UMG asset in the editor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
+	TSubclassOf<class UUserWidget> HUDOverlayAsset;
+
+	// Variable to hold the widget after creating it
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
+	UUserWidget* HUDOverlay;
+	
+protected:
+	
+	virtual void BeginPlay() override;
+};
